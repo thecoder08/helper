466b2b71f7a23b279809d870c92183ab870d9b73
Lennon McLean <lmmclean08@gmail.com>
2020-08-29 19:13:27 -0400

    added more responses

--- helper.js
+++ helper.js
@@ -5,14 +5,27 @@ console.log('Hi. I\'m HELPER. If you need me, you\'re probably not very smart.')
 process.stdin.on('data', function(data) {
   var message = data.toString().split(require('os').EOL)[0].toLowerCase();
   var words = message.split(' ');
-  if (words.includes('poop')) {
+  if (words.includes('poop') || words.includes('pooped') || words.includes('pooping')) {
     console.log('OK, opening the wikipedia page for "poop" in your default browser...');
     for (var i = 0; i < 10; i++) {
       open('https://en.wikipedia.org/wiki/Feces');
     }
   }
+  if (words.includes('fart') || words.includes('farted') || words.includes('farting')) {
+    console.log('OK, opening the wikipedia page for "fart" in your default browser...');
+    open('https://en.wikipedia.org/wiki/Flatulence');
+  }
+  if (words.includes('hi')) {
+    console.log('Bye!');
+    setInterval(function() {
+      process.exit();
+    }, 3000);
+  }
   if (words.includes('meaning') && words.includes('life')) {
-    console.log('um 43 I guess...');
+    console.log('43.');
+  }
+  if (words.includes('name')) {
+    console.log('HELPER. (duh)');
   }
   if (words.includes('hello')) {
     console.log('*sigh*, What do you want now...');

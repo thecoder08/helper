9d8959ce471302f2067d0185452d8e41b16ef94f
Lennon McLean <lmmclean08@gmail.com>
2021-03-07 09:45:00 -0500

    added audio

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 var cp = require('child_process');
 var fs = require('fs');
 var path = require('path');
-var helper = cp.spawn('node', ['helper.js']);
+var helper = cp.exec('node helper.js');
+function textToSpeech(text, callback) {
+  var audioinstance = cp.exec('node audio.js "' + text + '"');
+  console.log('node audio.js "' + text + '"');
+  audioinstance.stderr.on('data', function(data) {
+    console.err(data);
+  });
+  audioinstance.stdout.on('data', function(data) {
+    console.log(data);
+  });
+  audioinstance.on('close', function() {
+    callback();
+  });
+}
 if (confirm('Would you like to improve HELPER by sending data to the developers?')) {
   alert('Thanks for improving HELPER!');
   var sendData = true;
@@ -24,6 +37,13 @@ helper.stdout.on('data', function(data) {
     fs.appendFileSync(path.join(require('os').homedir(), '.helperdata.txt'), 'HELPER: ' + data.toString());
   }
   $('#answer').value = data.toString();
+  textToSpeech(data.toString(), function() {
+    console.log('callback!');
+    var audio = new Audio('output.mp3?foo=' + new Date().getTime());
+    audio.addEventListener("canplaythrough", function() {
+      audio.play();
+    });
+  });
 });
 helper.on('exit', function() {
   process.exit();
